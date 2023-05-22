2fcef1fbe2d2333567854afb2c3b900b2f64a529
kevin-llps <llopis.kevin@outlook.fr>
2023-05-22 04:25:28 +0200

    feat: Init project

---
+++ src/main/resources/sql/energy_classification.sql
@@ -0,0 +1,7 @@
+INSERT INTO rent.energy_classification (id, designation) VALUES (0x31000000000000000000000000000000, 'A');
+INSERT INTO rent.energy_classification (id, designation) VALUES (0xEFBBBF32000000000000000000000000, 'B');
+INSERT INTO rent.energy_classification (id, designation) VALUES (0xEFBBBF33000000000000000000000000, 'C');
+INSERT INTO rent.energy_classification (id, designation) VALUES (0xEFBBBF34000000000000000000000000, 'D');
+INSERT INTO rent.energy_classification (id, designation) VALUES (0xEFBBBF35000000000000000000000000, 'E');
+INSERT INTO rent.energy_classification (id, designation) VALUES (0xEFBBBF36000000000000000000000000, 'F');
+INSERT INTO rent.energy_classification (id, designation) VALUES (0xEFBBBF37000000000000000000000000, 'G');
---
+++ src/main/resources/sql/property_type.sql
@@ -0,0 +1,2 @@
+INSERT INTO rent.property_type (id, designation) VALUES (0xEFBBBF31000000000000000000000000, 'Appartement');
+INSERT INTO rent.property_type (id, designation) VALUES (0xEFBBBF32000000000000000000000000, 'Maison');
