57903249eef5b8d31b998ec0b565bb981ca49607
kevin-llps <llopis.kevin@outlook.fr>
2023-06-14 15:00:57 +0200

    fix: Optimize validation constraints on request DTO + Add Jakarta Bean Validation Provider

--- src/main/java/fr/esgi/rent/dto/request/RentalPropertyRequestDto.java
+++ src/main/java/fr/esgi/rent/dto/request/RentalPropertyRequestDto.java
@@ -1,5 +1,6 @@
 package fr.esgi.rent.dto.request;
 
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
 import jakarta.validation.constraints.NotNull;
@@ -10,17 +11,17 @@ public record RentalPropertyRequestDto(
         @NotNull String town,
         @NotNull String address,
         @NotNull String propertyType,
-        @NotNull double rentAmount,
-        @NotNull double securityDepositAmount,
-        @NotNull double area,
-        @NotNull int bedroomsCount,
+        @Min(1) double rentAmount,
+        @Min(1) double securityDepositAmount,
+        @Min(1) double area,
+        @Min(1) int bedroomsCount,
         int floorNumber,
         int numberOfFloors,
-        @NotNull int constructionYear,
+        @Min(1900) int constructionYear,
         @NotNull String energyClassification,
-        @NotNull boolean hasElevator,
-        @NotNull boolean hasIntercom,
-        @NotNull boolean hasBalcony,
-        @NotNull boolean hasParkingSpace) {
+        @NotNull Boolean hasElevator,
+        @NotNull Boolean hasIntercom,
+        @NotNull Boolean hasBalcony,
+        @NotNull Boolean hasParkingSpace) {
 
 }
